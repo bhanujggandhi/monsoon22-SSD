6631b298647dcff4b7ec623e8aa48beb91f430b8
bhanujggandhi <gandhibhanuj@gmail.com>
2022-08-23 00:34:20 +0530

    q2: last digit and all four classes should be atleast 1

--- ssd_assignment_1/q2.sh
+++ ssd_assignment_1/q2.sh
@@ -11,62 +11,77 @@ special=("~" "@" "%" "^" "!" "#" "%" "&" "(" ")" "_" "-" "+" "=" "," "." "<" ">"
 # Total 90 character
 allchar=(${digits[*]} ${lowercase[*]} ${uppercase[*]} ${special[*]})
 
-password=()
+flag=1
 
-while [[ $count -ne $size ]]; do
-  randno=$(($RANDOM % 89))
+while [[ $flag -eq 1 ]]; do
+  password=()
+  count=0
+  while [[ $count -ne $size ]]; do
+    randno=$(($RANDOM % 89))
 
-  # Starting should be either lowercase or uppercase
-  if [[ $count -eq 0 ]]; then
-    startarr=(${lowercase[*]} ${uppercase[*]})
-    randno=$(($RANDOM % 51))
-    password+=(${startarr[$randno]})
+    # Starting should be either lowercase or uppercase
+    if [[ $count -eq 0 ]]; then
+      startarr=(${lowercase[*]} ${uppercase[*]})
+      randno=$(($RANDOM % 51))
+      password+=(${startarr[$randno]})
 
-  # Ending should not be with an uppercase and no palindrome
-  elif [[ $count -eq $(($size - 1)) ]]; then
-    endarr=(${lowercase[*]} ${digits[*]} ${special[*]})
-    randno=$(($RANDOM % 63))
-    while [[ "${password[0]}" == "${endarr[$randno]}" ]]; do
-      randno=$(($RANDOM % 63))
-    done
-    password+=(${endarr[$randno]})
+    # Two same lowercase should not be together
+    elif [[ "${password[$(($count - 1))]}" == "${allchar[$randno]}" && "${lowercase[*]}" =~ "${allchar[$randno]}" ]]; then
+      char=${password[$(($count - 1))]}
+      while [[ "$char" == "${allchar[$randno]}" ]]; do
+        randno=$(($RANDOM % 89))
+      done
+      password+=(${allchar[randno]})
 
-  # Two same lowercase should not be together
-  elif [[ "${lowercase[*]}" =~ "${allchar[$randno]}" && "${password[$(($count - 1))]}" == "${allchar[$randno]}" ]]; then
-    char=${password[$(($count - 1))]}
-    while [[ "$char" == "${allchar[$randno]}" ]]; do
-      echo $char
-      echo ${allchar[$rando]}
-      randno=$(($RANDOM % 89))
-    done
-    password+=(${allchar[randno]})
+      # Lowercase should not be with a digit
+    elif [[ "${lowercase[*]}" =~ "${password[$(($count - 1))]}" && "${digits[*]}" =~ "${allchar[$randno]}" ]]; then
+      while [[ "${digits[*]}" =~ "${allchar[$randno]}" ]]; do
+        randno=$(($RANDOM % 89))
+      done
+      password+=(${allchar[randno]})
 
-    # Lowercase should not be with a digit
-  elif [[ "${lowercase[*]}" =~ "${password[$(($count - 1))]}" && "${digits[*]}" =~ "${allchar[$randno]}" ]]; then
-    while [[ "${digits[*]}" =~ "${allchar[$randno]}" ]]; do
-      echo ${allchar[$rando]}
-      randno=$(($RANDOM % 89))
-    done
-    password+=(${allchar[randno]})
+    elif [[ "${digits[*]}" =~ "${password[$(($count - 1))]}" && "${lowercase[*]}" =~ "${allchar[$randno]}" ]]; then
+      while [[ "${lowercase[*]}" =~ "${allchar[$randno]}" ]]; do
+        randno=$(($RANDOM % 89))
+      done
+      password+=(${allchar[randno]})
 
-  elif [[ "${digits[*]}" =~ "${password[$(($count - 1))]}" && "${lowercase[*]}" =~ "${allchar[$randno]}" ]]; then
-    while [[ "${lowercase[*]}" =~ "${allchar[$randno]}" ]]; do
-      echo ${allchar[$rando]}
-      randno=$(($RANDOM % 89))
-    done
-    password+=(${allchar[randno]})
+      # 2 symbols not together
+    elif [[ "${special[*]}" =~ "${password[$(($count - 1))]}" && "${special[*]}" =~ "${allchar[$randno]}" ]]; then
+      while [[ "${special[*]}" =~ "${allchar[$randno]}" ]]; do
+        randno=$(($RANDOM % 89))
+      done
+      password+=(${allchar[randno]})
+    else
+      password+=(${allchar[randno]})
+    fi
+    count=$(($count + 1))
+  done
 
-    # 2 symbols not together
-  elif [[ "${special[*]}" =~ "${password[$(($count - 1))]}" && "${special[*]}" =~ "${allchar[$randno]}" ]]; then
-    while [[ "${special[*]}" =~ "${allchar[$randno]}" ]]; do
-      echo ${allchar[$rando]}
-      randno=$(($RANDOM % 89))
-    done
-    password+=(${allchar[randno]})
+  lowercount=0
+  uppercount=0
+  specialcount=0
+  digitcount=0
+
+  for ((i = 0; i < $size; i++)); do
+    if [[ "${special[@]}" =~ "${password[$i]}" ]]; then
+      specialcount=$(($specialcount + 1))
+    elif [[ "${lowercase[@]}" =~ "${password[$i]}" ]]; then
+      lowercount=$(($lowercount + 1))
+    elif [[ "${uppercase[@]}" =~ "${password[$i]}" ]]; then
+      uppercount=$(($uppercount + 1))
+    elif [[ "${digits[@]}" =~ "${password[$i]}" ]]; then
+      digitcount=$(($digitcount + 1))
+    fi
+  done
+
+  if [[ $lowercount -eq 0 || $uppercount -eq 0 || $specialcount -eq 0 || $digitcount -eq 0 || "${uppercase[@]}" =~ "${password[$(($size - 1))]}" ]]; then
+    flag=1
+    continue
   else
-    password+=(${allchar[randno]})
+    flag=0
+    break
   fi
-  count=$(($count + 1))
 done
 
 echo "${password[*]}"
