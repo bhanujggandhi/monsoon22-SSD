a9335553e33aa13e579ce9a7d7a9eff37f6dcb8f
bhanujggandhi <gandhibhanuj@gmail.com>
2022-10-07 08:45:03 +0530

    Q1 map created

--- ssd_assignment_3/q1/script.js
+++ ssd_assignment_3/q1/script.js
@@ -1,3 +1,30 @@
+// Adj to 0 index are 1 and 7
+const adjacents = [
+  "17",
+  "0237",
+  "13",
+  "1249",
+  "39",
+  "89",
+  "78",
+  "0168",
+  "5679",
+  "3458",
+];
+
+const elpToCrow = {
+  elp0: "",
+  elp1: "",
+  elp2: "",
+  elp3: "",
+  elp4: "",
+  elp5: "",
+  elp6: "",
+  elp7: "",
+  elp8: "",
+  elp9: "",
+};
+
 const droppable = (e) => {
   e.preventDefault();
 };
@@ -9,6 +36,10 @@ const drag = (e) => {
 const drop = (e) => {
   e.preventDefault();
   const data = e.dataTransfer.getData("text");
-  if (!e.target.id.match(/crow*/g) && !e.target.id.match(/vulture/g))
+  // if (!e.target.id.match(/crow*/g) && !e.target.id.match(/vulture/g)) {
+  if (e.target.id in elpToCrow && elpToCrow[e.target.id] === "") {
     e.target.appendChild(document.getElementById(data));
+    elpToCrow[e.target.id] = data;
+    console.log(JSON.stringify(elpToCrow));
+  }
 };
