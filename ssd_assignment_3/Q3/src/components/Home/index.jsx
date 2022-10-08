19e936846c243e5e6a6dc3fa94c08133814d9e17
bhanujggandhi <gandhibhanuj@gmail.com>
2022-10-08 20:20:43 +0530

    Q3 Search Input Created

--- ssd_assignment_3/Q3/src/components/Home/index.jsx
+++ ssd_assignment_3/Q3/src/components/Home/index.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header/Header";
+import Search from "./Search/Search";
+
+const searchExec = () => {};
+const clearSearch = () => {};
+const handleSearchInp = (e) => {
+  setSearchInp(e.target.value);
+};
 
 function Home() {
+  const [searchInp, setSearchInp] = useState("");
   return (
     <div>
       {/* Page Header */}
       <Header />
 
       {/* Search Bar */}
+      <Search
+        clearSearch={clearSearch}
+        searchExec={searchExec}
+        searchinp={searchInp}
+        handleSearchInp={handleSearchInp}
+      />
 
       {/* News List */}
     </div>
