3688527ef284ec28e6c295ed7ee248218aadae8a
bhanujggandhi <gandhibhanuj@gmail.com>
2022-10-28 09:24:58 +0530

    q2 search in the dictionary

--- ssd_assignment_4/q2.py
+++ ssd_assignment_4/q2.py
@@ -1,9 +1,15 @@
-from csv import writer
+from csv import writer, reader
 import pandas as pd
 
 
 while True:
-    print("Enter the task input")
+    print("Enter the task input:")
+    print("1. Add new entry to the Address Dictionary")
+    print("2. Show all the entries in the dictionary")
+    print("3. Update an entry")
+    print("4. Remove an entry")
+    print("5. Search in the dictionary")
+    print("6. Quit")
     task = int(input())
     if task == 1:
         List = []
@@ -28,10 +34,28 @@ while True:
             writer_object.writerow(List)
             f_object.close()
     
-    elif task == 3:
+    elif task == 2:
         df = pd.read_csv("data.csv")
+        df = pd.read_csv('data.csv',
+            header=0, 
+            names=['First Name', 'Last Name','Address', 'City', 'State', 'Zip Code', 'Contact Number', 'Email Address'])
         pd.options.display.max_rows = 9999
         print(df)
     
-    elif task == 0:
+    elif task == 5:
+        query = input('Enter number to find\n')
+
+        df = reader(open('data.csv', "r"))
+
+        res = []
+
+        for row in df:
+            for entry in row:
+                if query in entry:
+                    res.append(row)
+
+        print(pd.DataFrame(res, 
+            columns=['First Name', 'Last Name','Address', 'City', 'State', 'Zip Code', 'Contact Number', 'Email Address']))
+    
+    elif task == 6:
         break
