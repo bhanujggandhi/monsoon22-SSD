38e495fc4c14983b22dc8c2062bda222b9a10949
bhanujggandhi <gandhibhanuj@gmail.com>
2022-11-02 01:18:03 +0530

    validation added

--- ssd_assignment_4/q2.py
+++ ssd_assignment_4/q2.py
@@ -1,6 +1,35 @@
 from csv import writer, reader, DictReader, DictWriter
 import pandas as pd
 from tabulate import tabulate
+import re
+
+
+def checkemail(email):
+    Pattern = re.compile(
+        r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    m = re.match(Pattern, email)
+    if m is None:
+        return False
+    else:
+        return True
+
+
+def checknumber(num):
+    Pattern = re.compile("(0|91)?[6-9][0-9]{9}")
+    return Pattern.match(num)
+
+
+def checkpostal(pin):
+    regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$"
+    Pattern = re.compile(regex)
+    if (pin == ''):
+        return False
+    m = re.match(Pattern, pin)
+    if m is None:
+        return False
+    else:
+        return True
+
 
 while True:
     print("Enter the task input:")
@@ -24,11 +53,23 @@ while True:
         print("Enter state: ")
         List.append(input())
         print("Enter zip: ")
-        List.append(input())
+        postal = input()
+        while checkpostal(postal) == False:
+            print("Please enter a valid Zip Code")
+            postal = input()
+        List.append(postal)
         print("Enter contact number: ")
-        List.append(input())
+        num = input()
+        while checknumber(num) == False:
+            print("Please enter a valid Phone Number")
+            num = input()
+        List.append(num)
         print("Enter email: ")
-        List.append(input())
+        email = input()
+        while checkemail(email) == False:
+            print("Please enter a valid email")
+            postal = input()
+        List.append(email)
         with open('data.csv', 'a') as f_object:
             writer_object = writer(f_object)
             writer_object.writerow(List)
@@ -54,6 +95,20 @@ while True:
         print("Enter a new value: ")
         newval = input()
 
+        if loc == 6:
+            while checkpostal(newval) == False:
+                print("Please enter a valid Zip Code")
+                newval = input()
+
+        if loc == 7:
+            while checknumber(newval) == False:
+                print("Please enter a valid contact number")
+                newval = input()
+        if loc == 8:
+            while checkemail(newval) == False:
+                print("Please enter a valid email address")
+                newval = input()
+
         with open("data.csv", "r") as file:
             readData = [row for row in DictReader(file)]
             readData[ind][ops[loc]] = newval
@@ -61,10 +116,9 @@ while True:
         readHeader = readData[0].keys()
 
         with open("data.csv", "w") as file:
-            writer = DictWriter(file, fieldnames = readHeader)
+            writer = DictWriter(file, fieldnames=readHeader)
             writer.writeheader()
             writer.writerows(readData)
-
 
     elif task == 4:
         print("Enter the entry that you want to delete: ")
@@ -77,10 +131,9 @@ while True:
         readHeader = readData[0].keys()
 
         with open("data.csv", "w") as file:
-            writer = DictWriter(file, fieldnames = readHeader)
+            writer = DictWriter(file, fieldnames=readHeader)
             writer.writeheader()
             writer.writerows(readData)
-
 
     elif task == 5:
         query = input('Enter query to find\n')
@@ -94,7 +147,7 @@ while True:
                 if query in entry:
                     res.append(row)
         print(tabulate(pd.DataFrame(res,
-                           columns=['First Name', 'Last Name', 'Address', 'City', 'State', 'Zip Code', 'Contact Number', 'Email Address']),headers="keys", tablefmt="fancy_outline"))
+                                    columns=['First Name', 'Last Name', 'Address', 'City', 'State', 'Zip Code', 'Contact Number', 'Email Address']), headers="keys", tablefmt="fancy_outline"))
 
     elif task == 6:
         break
--- ssd_assignment_4/q3.py
+++ ssd_assignment_4/q3.py
@@ -2,134 +2,162 @@
 from matplotlib import pyplot as plt
 from matplotlib import style
 import math
+from termcolor import colored
+
 
 style.use("ggplot")
 
 
 numeric = '0'
 
-xplots=[0]
-yplots=[0]
+xplots = [0]
+yplots = [0]
 
 total_dist = 0
 finalx = 0
 finaly = 0
 
-def parselen(value):
-  units = ""
-  number = 0
-  while value:
-    try:
-      number = float(value)
-      break
-    except ValueError:
-      units = value[-1:] + units
-      value = value[:-1]
-  return number, units.strip()
 
+def parselen(value):
+    units = ""
+    number = 0
+    while value:
+        try:
+            number = float(value)
+            break
+        except ValueError:
+            units = value[-1:] + units
+            value = value[:-1]
+    return number, units.strip()
 
 
 while True:
-  inp = input()
+    print("Please enter an input (eg: 1mm N) or 0 to stop")
+    inp = input()
 
-  splitinp = inp.split()
+    splitinp = inp.split()
 
-  if len(splitinp) == 1:
-    if splitinp[0] == '0':
-      break
+    if len(splitinp) == 1:
+        if splitinp[0] == '0':
+            break
 
-  length, unit = parselen(splitinp[0])
+    length, unit = parselen(splitinp[0])
 
-  if len(splitinp) <= 1 or len(splitinp) > 2 or unit == "" or unit not in ["mm", "cm", "km"] or splitinp[1] not in ["N", "NE", "NW", "E", "W", "S", "SE", "SW"]:
-    print("Please enter a valid input, eg: 1mm N")
-    continue
+    if len(splitinp) <= 1 or len(splitinp) > 2 or unit == "" or unit not in ["mm", "cm", "m", "km"] or splitinp[1] not in ["N", "NE", "NW", "E", "W", "S", "SE", "SW"]:
+        print("Please enter a valid input, eg: 1mm N")
+        continue
 
-  if splitinp[1] == "NE":
-    tempx = (length * 0.7071) + xplots[-1]
-    tempy = (length * 0.7071) + yplots[-1]
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
-  elif splitinp[1] == "NW":
-    tempx = xplots[-1] + (length * -0.7071)
-    tempy = yplots[-1] + (length * 0.7071)
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
-  elif splitinp[1] == "SE":
-    tempx = xplots[-1] + (length * 0.7071)
-    tempy = yplots[-1] + (length * -0.7071)
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
-  elif splitinp[1] == "SW":
-    tempx = xplots[-1] + (length * -0.7071)
-    tempy = yplots[-1] + (length * -0.7071)
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
-  elif splitinp[1] == "N":
-    tempx = xplots[-1] + (length * 0)
-    tempy = yplots[-1] + (length * 1)
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
-  elif splitinp[1] == "S":
-    tempx = xplots[-1] + (length * 0)
-    tempy = yplots[-1] + (length * -1)
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
-  elif splitinp[1] == "E":
-    tempx = xplots[-1] + (length * 1)
-    tempy = yplots[-1] + (length * 0)
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
-  elif splitinp[1] == "W":
-    tempx = xplots[-1] + (length * -1)
-    tempy = yplots[-1] + (length * 0)
-    line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],'o',ls='-', ms=8,markevery=[-1], label=inp)
-    xplots.append(tempx)
-    yplots.append(tempy)
-    total_dist += length
-    finalx = tempx
-    finaly = tempy
-    print("You are at: ", "(", tempx, tempy, ")")
+    if unit == "cm":
+        length = length * 10
+    elif unit == "m":
+        length = length * 1000
+    elif unit == "km":
+        length = length * 100000
 
-print("You have travelled a total distance of", total_dist, "mm")
+    if splitinp[1] == "NE":
+        tempx = (length * 0.7071) + xplots[-1]
+        tempy = (length * 0.7071) + yplots[-1]
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+    elif splitinp[1] == "NW":
+        tempx = xplots[-1] + (length * -0.7071)
+        tempy = yplots[-1] + (length * 0.7071)
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+    elif splitinp[1] == "SE":
+        tempx = xplots[-1] + (length * 0.7071)
+        tempy = yplots[-1] + (length * -0.7071)
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+    elif splitinp[1] == "SW":
+        tempx = xplots[-1] + (length * -0.7071)
+        tempy = yplots[-1] + (length * -0.7071)
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+    elif splitinp[1] == "N":
+        tempx = xplots[-1] + (length * 0)
+        tempy = yplots[-1] + (length * 1)
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+    elif splitinp[1] == "S":
+        tempx = xplots[-1] + (length * 0)
+        tempy = yplots[-1] + (length * -1)
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+    elif splitinp[1] == "E":
+        tempx = xplots[-1] + (length * 1)
+        tempy = yplots[-1] + (length * 0)
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+    elif splitinp[1] == "W":
+        tempx = xplots[-1] + (length * -1)
+        tempy = yplots[-1] + (length * 0)
+        line = plt.plot([xplots[-1], tempx], [yplots[-1], tempy],
+                        'o', ls='-', ms=8, markevery=[-1], label=inp)
+        xplots.append(tempx)
+        yplots.append(tempy)
+        total_dist += length
+        finalx = tempx
+        finaly = tempy
+        print("You are at: ", colored("(" + str(tempx)+", " +
+              str(tempy) + ")", "red", attrs=["bold", "reverse", "underline"]))
+
+print("You have travelled a total distance of", colored(total_dist, "green", attrs=[
+      "bold", "reverse", "underline"]), colored("mm", "green", attrs=["bold", "reverse", "underline"]))
 
 angrad = math.atan2(finaly, finalx)
 
-print("You are at a distance (Euclidean) of", math.dist([0,0], [finalx, finaly]), "mm from (0, 0) with an angle of", math.degrees(angrad), "degrees.")
+print("You are at a distance (Euclidean) of", colored(math.dist([0, 0], [
+      finalx, finaly]), "green", attrs=["bold", "reverse", "underline"]), colored("mm", "green", attrs=["bold", "reverse"]), "from (0, 0) with an angle of", colored(math.degrees(angrad), "green", attrs=["bold", "reverse", "underline"]), colored("degrees.", "green", attrs=["bold", "reverse"]))
 
 
 plt.legend()
