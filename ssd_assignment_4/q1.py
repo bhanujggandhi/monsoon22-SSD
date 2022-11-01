ee1fc4ca9a8ca6a9dde3845ce573ea45339af470
bhanujggandhi <gandhibhanuj@gmail.com>
2022-11-01 09:54:55 +0530

    q1 completed

--- ssd_assignment_4/q1.py
+++ ssd_assignment_4/q1.py
@@ -1,12 +1,27 @@
-print("  _________________")
-print(" /                 \\")
-print("/                   \\")
-print()
-print("+-------------------+")
+topline = "  ______ "
+linewithsym = "+--------+"
+pyramidlevel1 = " /      \ "
+pyramidlevel2 = "/        \ "
+bottomline = " \______/ "
+pyramidlevel3 = "\        / "
+message = "|  STOP  | "
 
-print("  _________________")
-print(" /                 \\")
-print("/                   \\")
-print("\                   /")
-print(" \                 /")
-print("  _________________")
+
+print(topline)
+print(pyramidlevel1)
+print(pyramidlevel2)
+print(linewithsym)
+print(topline)
+print(pyramidlevel1)
+print(pyramidlevel2)
+print(pyramidlevel3)
+print(bottomline)
+print(pyramidlevel3)
+print(bottomline)
+print(linewithsym)
+print(topline)
+print(pyramidlevel1)
+print(pyramidlevel2)
+print(message)
+print(pyramidlevel3)
+print(bottomline)
